1201b21989ad0fabfc0832f0debc3dd1fbb99f72
James-Boarati <runaway_james@hotmail.com>
2023-09-14 08:10:58 -0300

    corrigindo erro (document)

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -3,7 +3,7 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
-    const categoria = documento.elementoBtn.value
+    const categoria = elementoBtn.value
     let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     console.table(livrosFiltrados)
 }
