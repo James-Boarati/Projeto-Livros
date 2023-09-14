b0fdb9cf69bea9f9f27476abdf69085b35845888
James-Boarati <runaway_james@hotmail.com>
2023-09-14 08:08:36 -0300

    Filter de livros no console

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -2,7 +2,8 @@ const botoes = document.querySelectorAll('.btn')
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
-
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = documento.elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     console.table(livrosFiltrados)
 }
