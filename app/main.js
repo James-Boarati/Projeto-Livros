e7dfc9d263ea1d4e0146c83d2629e6e010dc91f6
James-Boarati <runaway_james@hotmail.com>
2023-09-13 22:29:03 -0300

    Desconto em livros usando: map

--- app/main.js
+++ app/main.js
@@ -1,29 +1,10 @@
 let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
-const elementoParaInserirlivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI) 
     livros = await res.json()
-    console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
-
-function exibirOsLivrosNaTela (listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirlivros.innerHTML += `<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
-          alt="${livro.alt}" />
-        <h2 class="libro__titulo">
-        ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>`
-    })
-}
-
